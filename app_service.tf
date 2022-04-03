5ce463dcfdb1c821cfcbe5dd28d5ceb1ef6d0f3c
arkadiuss <arkadius.kraus@gmail.com>
2022-04-03 02:25:06 +0200

    import cognitive and add app service

--- app_service.tf
+++ app_service.tf
@@ -8,16 +8,35 @@ resource "azurerm_app_service_plan" "hacknarog" {
     size = "S1"
   }
 }
-
-resource "azurerm_app_service" "example" {
-  name                = "example-app-service"
-  location            = data.azurerm_resource_group.hacknarog.location
-  resource_group_name = data.azurerm_resource_group.hacknarog.name
-  app_service_plan_id = azurerm_app_service_plan.hacknarog.id
-
-  site_config {
-    linux_fx_version = "COMPOSE|${base64encode(templatefile("docker-compose.yml", {
-      MONGO_URI = azurerm_cosmosdb_account.hacknarog.connection_strings[0]
-    }))}"
-  }
+output "test" {
+  value = templatefile("docker-compose.yml", {
+    MONGO_URI = azurerm_cosmosdb_account.hacknarog.connection_strings[0]
+  })
+  sensitive = true
 }
+
+output "test2" {
+  value = base64encode(templatefile("docker-compose.yml", {
+    MONGO_URI = azurerm_cosmosdb_account.hacknarog.connection_strings[0]
+  }))
+  sensitive = true
+}
+#resource "azurerm_app_service" "hacknarog" {
+#  name                = "Forsetihacknarog"
+#  location            = "centralus"
+#  resource_group_name = data.azurerm_resource_group.hacknarog.name
+#  app_service_plan_id = azurerm_app_service_plan.hacknarog.id
+#
+#  app_settings = {
+#    DOCKER_REGISTRY_SERVER_USERNAME     = azurerm_container_registry.acr.admin_username
+#    DOCKER_REGISTRY_SERVER_PASSWORD     = azurerm_container_registry.acr.admin_password
+#    DOCKER_REGISTRY_SERVER_URL          = azurerm_container_registry.acr.login_server
+#    WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+#  }
+#
+#  site_config {
+#    linux_fx_version = "COMPOSE|${base64encode(templatefile("docker-compose.yml", {
+#      MONGO_URI = azurerm_cosmosdb_account.hacknarog.connection_strings[0]
+#    }))}"
+#  }
+#}
--- cognitive.tf
+++ cognitive.tf
@@ -1,7 +1,14 @@
 resource "azurerm_cognitive_account" "hacknarog" {
-  name                = "forseti-text-analysis-account"
-  location            = data.azurerm_resource_group.hacknarog.location
-  resource_group_name = data.azurerm_resource_group.hacknarog.name
-  kind                = "TextAnalytics"
-  sku_name            = "S0"
+  custom_subdomain_name = "forseti-text-analysis-account"
+  name                  = "forseti-text-analysis-account"
+  location              = "East US"
+  resource_group_name   = data.azurerm_resource_group.hacknarog.name
+  kind                  = "TextAnalytics"
+  sku_name              = "F0"
+
+  network_acls {
+    default_action = "Allow"
+    ip_rules       = []
+  }
+
 }
